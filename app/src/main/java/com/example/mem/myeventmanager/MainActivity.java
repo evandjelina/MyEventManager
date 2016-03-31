98b72ea54053cc7a70596b74679b7a4afdb68f8a
evandjelina <velickovic.evandjelina@gmail.com>
2016-03-31 11:36:34 +0200

    Novijaa verzija v1.0

---
+++ app/src/main/java/com/example/mem/myeventmanager/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.mem.myeventmanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+    }
+    public void addActivity(View view) {
+        if(view.getId()==R.id.addActivity){
+            Intent intent = new Intent(this, AddActivity.class);
+            startActivity(intent);
+        }
+
+    }
+// otvara listu
+    public void listActivity(View view) {
+        if(view.getId()==R.id.manageActivities){
+            Intent intent = new Intent(this, ListActivities.class);
+            startActivity(intent);
+        }
+
+    }
+
+    public void openCal(View view) {
+        if(view.getId()==R.id.openCalendar){
+            Intent intent = new Intent(this, OpenCal.class);
+            startActivity(intent);
+        }
+
+    }
+
+    public void exitApp(View view){
+        if(view.getId()==R.id.exitApp){
+            this.finish();
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
