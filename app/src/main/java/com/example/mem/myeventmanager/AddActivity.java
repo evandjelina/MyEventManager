4fca9167aa40e4810fd7a999ab2945c1d73fff96
evandjelina <velickovic.evandjelina@gmail.com>
2016-06-21 00:42:23 +0200

    Gotova app

--- app/src/main/java/com/example/mem/myeventmanager/AddActivity.java
+++ app/src/main/java/com/example/mem/myeventmanager/AddActivity.java
@@ -1,45 +1,37 @@
 package com.example.mem.myeventmanager;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
+
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.os.Vibrator;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
 
 /**
  * Created by Djina on 30.3.2016.
  */
 public class AddActivity extends Activity{
 
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_activity);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri = Uri.parse("rtsp://r5---sn-4g57knl6.googlevideo.com/Cj0LENy73wIaNAmyH1fWEr3v1hMYDSANFC3FJmhXMOCoAUIASARg2brB8ZW9h7RXigELV19aMGVsOElKeUUM/7A459B49B319C09C39BBEF82AA112272075C14ED.70ABDCC39BF99F7045162C472E5EF377FA3151C3/yt6/1/video.3gp");
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
+
+
     }
 
-    EditText message_text;
-    public final static String MESSAGE_KEY = "com.example.mem.myeventmanager.message_key";
-
-    public void sendMessage(View views)
-    {
-        message_text = (EditText)findViewById(R.id.message_text);
-        String message=message_text.getText().toString();
-        Intent intent = new Intent(this, ListActivities.class);
-        intent.putExtra(MESSAGE_KEY, message);
-        startActivity(intent);
-    }
-
-    public void cancel(View view){
-        if(view.getId()==R.id.btnCancel){
-            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            v.vibrate(50);
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        }
-    }
 
 
 
--- app/src/main/java/com/example/mem/myeventmanager/ListActivities.java
+++ app/src/main/java/com/example/mem/myeventmanager/ListActivities.java
@@ -1,25 +1,204 @@
 package com.example.mem.myeventmanager;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.os.Vibrator;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ListAdapter;
-import android.widget.TextView;
+import android.view.ViewGroup;
+
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
-import java.util.ArrayList;
-
-/**
- * Created by Djina on 31.3.2016.
- */
 public class ListActivities extends Activity {
 
-    ListView listView;
+    //dugme za vracanje u meni, sa vibracijom od 50
+    public void cancelAct(View view){
+        if(view.getId()==R.id.btnCancel){
+            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(50);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+    MyCustomAdapter dataAdapter = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_activities);
+
+        //Generate list View from ArrayList
+        displayListView();
+
+        checkButtonClick();
+
+    }
+
+    private void displayListView() {
+
+        ArrayList<Aktivnost> aktivnosti = new ArrayList<Aktivnost>();
+
+        Aktivnost akt1 = new Aktivnost("dan 1", "Napravi cilj projekta!", false);
+        aktivnosti.add(akt1);
+
+        Aktivnost akt2 = new Aktivnost("dan 3", "Utvrdi podelu poslova!", false);
+        aktivnosti.add(akt2);
+
+        Aktivnost akt3 = new Aktivnost("dan 5", "Organizuj tim!", false);
+        aktivnosti.add(akt3);
+
+        Aktivnost akt4 = new Aktivnost("dan 9", "Nađi sponzore!", false);
+        aktivnosti.add(akt4);
+
+        Aktivnost akt5 = new Aktivnost("dan 9", "Organizuj promociju!", false);
+        aktivnosti.add(akt5);
+
+        Aktivnost akt6 = new Aktivnost("dan 16", "Presek stanja!", false);
+        aktivnosti.add(akt6);
+
+        Aktivnost akt7 = new Aktivnost("dan 20", "Poslednje pripreme...", false);
+        aktivnosti.add(akt7);
+
+        Aktivnost akt8 = new Aktivnost("dan 25", "Uživaj u događaju! :)", false);
+        aktivnosti.add(akt8);
+
+        dataAdapter = new MyCustomAdapter(this,
+                R.layout.aktivnost_info, aktivnosti);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        // Assign adapter to ListView
+        listView.setAdapter(dataAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Aktivnost country = (Aktivnost) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Odradili ste: " + country.getName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private class MyCustomAdapter extends ArrayAdapter<Aktivnost> {
+
+        private ArrayList<Aktivnost> aktivnosti;
+
+        public MyCustomAdapter(Context context, int textViewResourceId,
+                               ArrayList<Aktivnost> aktivnosti) {
+            super(context, textViewResourceId, aktivnosti);
+            this.aktivnosti = new ArrayList<Aktivnost>();
+            this.aktivnosti.addAll(aktivnosti);
+        }
+
+        private class ViewHolder {
+            TextView code;
+            CheckBox name;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater) getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                convertView = vi.inflate(R.layout.aktivnost_info, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v;
+                        Aktivnost country = (Aktivnost) cb.getTag();
+                        Toast.makeText(getApplicationContext(),
+                                "Odradili ste: " + cb.getText() +
+                                        " " + cb.isChecked(),
+                                Toast.LENGTH_LONG).show();
+                        country.setSelected(cb.isChecked());
+                    }
+                });
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Aktivnost aktivnost = aktivnosti.get(position);
+            holder.code.setText(" (" + aktivnost.getCode() + ")");
+            holder.name.setText(aktivnost.getName());
+            holder.name.setChecked(aktivnost.isSelected());
+            holder.name.setTag(aktivnost);
+
+            return convertView;
+
+
+        }
+
+    }
+
+    private void checkButtonClick() {
+
+
+        Button myButton = (Button) findViewById(R.id.findSelected);
+        myButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer responseText = new StringBuffer();
+                responseText.append("Sledeće aktivnosti su urađene...\n");
+
+                ArrayList<Aktivnost> aktivnosti = dataAdapter.aktivnosti;
+                for(int i=0;i<aktivnosti.size();i++){
+                    Aktivnost aktivnost = aktivnosti.get(i);
+                    if(aktivnost.isSelected()){
+                        responseText.append("\n" + aktivnost.getName());
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(),
+                        responseText, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* ListView listView;
     ArrayAdapter<String> adapter;
     String [] android_versions= {"Jedan", "Dva", "Android"};
 
@@ -39,7 +218,7 @@ public class ListActivities extends Activity {
         /*android_versions[0]="Prooba";
 
         int s = returnPosition();
-        android_versions[s]=message;*/
+        android_versions[s]=message;
 
 
         listView = (ListView) findViewById(R.id.list_view);
@@ -74,14 +253,10 @@ public class ListActivities extends Activity {
         return i;
 
     }
+ */
 
-    public void cancel(View view){
-        if(view.getId()==R.id.btnCancel){
-            Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-            v.vibrate(50);
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        }
-    }
 
-}
+
+
+
+
